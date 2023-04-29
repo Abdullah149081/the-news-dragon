3f81a296e2b8b1f0e116c54f86b9080f3b0fe3e8
Abdullah149081 <masudmd425@gmail.com>
2023-04-29 23:46:07 +0600

    navbar add

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,5 +1,8 @@
 import moment from "moment";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Navbar from "react-bootstrap/Navbar";
 import Marquee from "react-fast-marquee";
 import logo from "../../../assets/logo.png";
 import "./Header.css";
@@ -21,6 +24,29 @@ const Header = () => {
         <Marquee speed={50} pauseOnClick className="news-highlight">
           I can be a React component, multiple React components, or just some text.
         </Marquee>
+      </div>
+      <div style={{ marginTop: "21px" }}>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Brand />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#pricing">Career</Nav.Link>
+              </Nav>
+              <Nav className="d-md-inline-flex align-items-md-center">
+                <Nav.Link href="#deets">profile</Nav.Link>
+                <Nav.Link className="" eventKey={2} to="/login">
+                  <button type="button" className="btn-dragon btn-login">
+                    login
+                  </button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
     </Container>
   );
