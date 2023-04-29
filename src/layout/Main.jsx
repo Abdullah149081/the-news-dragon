bd915230c14860fc1fdd1071647205754fd6e57a
Abdullah149081 <masudmd425@gmail.com>
2023-04-29 22:33:03 +0600

    header logo done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,15 +1,30 @@
+import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Header from "../pages/Shared/Header/Header";
+import LeftNav from "../pages/Shared/leftNav/LeftNav";
+import RightNav from "../pages/Shared/rightNav/RightNav";
 import "./Main.css";
 
 const Main = () => {
   return (
     <div>
       <Header />
-      <div className="outlet">
-        <Outlet />
-      </div>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <LeftNav />
+          </Col>
+          <Col lg={6}>
+            <h1>main section</h1>
+          </Col>
+          <Col lg={3}>
+            <RightNav />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,5 +1,19 @@
+import moment from "moment";
+import logo from "../../../assets/logo.png";
+import "./Header.css";
+
 const Header = () => {
-  return <div>This is header</div>;
+  return (
+    <div>
+      <div className="text-center mt-5">
+        <img src={logo} alt="logo" />
+        <p className="header-title">Journalism Without Fear or Favour</p>
+        <p className="time-date">
+          <span style={{ color: "#403f3f" }}>{moment().format("dddd")}</span>, <span style={{ color: "#706F6F" }}>{moment().format("MMMM D, YYYY")}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
