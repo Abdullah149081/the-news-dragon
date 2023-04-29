a3d10e31c3868c7aa6740d67ae52d7a7c54a17e1
Abdullah149081 <masudmd425@gmail.com>
2023-04-29 23:22:23 +0600

    react marquee add

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,12 @@
 import moment from "moment";
+import Container from "react-bootstrap/Container";
+import Marquee from "react-fast-marquee";
 import logo from "../../../assets/logo.png";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center mt-5">
         <img src={logo} alt="logo" />
         <p className="header-title">Journalism Without Fear or Favour</p>
@@ -12,7 +14,15 @@ const Header = () => {
           <span style={{ color: "#403f3f" }}>{moment().format("dddd")}</span>, <span style={{ color: "#706F6F" }}>{moment().format("MMMM D, YYYY")}</span>
         </p>
       </div>
-    </div>
+      <div className="d-flex justify-content-center align-items-center baking-news">
+        <button type="button" className="btn-dragon">
+          Latest
+        </button>
+        <Marquee speed={50} pauseOnClick className="news-highlight">
+          I can be a React component, multiple React components, or just some text.
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
