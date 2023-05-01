667ea74da161f49b25ba8ec564930de409d25bc6
Abdullah149081 <masudmd425@gmail.com>
2023-05-01 12:33:45 +0600

    login and register design done

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className=" login-container">
+      <h2 className="text-center login-title ">Login your account</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="login-text">Email address</Form.Label>
+          <Form.Control name="email" type="email" placeholder="Enter your email address" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label className="login-text">Password</Form.Label>
+          <Form.Control name="password" type="password" placeholder="Enter your password" />
+        </Form.Group>
+
+        <button type="button" className="btn-dragon login-btn">
+          login
+        </button>
+        <p className="text-center">
+          <span className="login-account">Dont’t Have An Account ?</span>
+          <span>
+            <Link className="news-link" to="/register">
+              Register
+            </Link>
+          </span>
+        </p>
+      </Form>
+      <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
     </div>
   );
 };
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className=" login-container">
+      <h2 className="text-center login-title ">Register your account</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="login-text">Your Name</Form.Label>
+          <Form.Control name="text" type="text" placeholder="Enter your name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="login-text">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email address" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="login-text">Photo URL</Form.Label>
+          <Form.Control type="file" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label className="login-text">Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your password" />
+        </Form.Group>
+
+        <button type="submit" className="btn-dragon login-btn">
+          Register
+        </button>
+        <p className="text-center">
+          <span className="login-account">Already Have An Account ?</span>
+          <span>
+            <Link className="news-link" to="/login">
+              login
+            </Link>
+          </span>
+        </p>
+      </Form>
+      <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
     </div>
   );
 };
